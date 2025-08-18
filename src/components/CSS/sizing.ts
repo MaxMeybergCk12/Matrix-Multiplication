b5d2a93dd531bce0e6acd2bed3f5bdb0f4f89931
MaxMeyberg <max@pitchmail.io>
2025-08-18 10:54:26 -0700

    reformatting for typing and good foundation

---
+++ src/components/CSS/sizing.ts
@@ -0,0 +1,5 @@
+// This file takes in 2 strings which are the sizes of the matrixces, 
+
+//Parses them then assigns how muh "space each of them is given
+
+//It is on a percentag ebases, depending on how many "elements we have"
--- src/components/Matrix/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as Matrix } from './Matrix.tsx';
-export { default as MatrixCell } from './MatrixCell.tsx';
-export { default as MatrixBrackets } from './MatrixBrackets.tsx';
