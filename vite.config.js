5b03ace5b3e6938f9b1ed66bf9d44045fe1400c6
MaxMeyberg <max@pitchmail.io>
2025-08-26 14:24:37 -0700

    adding to gh pages

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react()],
+    base: '/Matrix_Multiplication/',
+    build: {
+        outDir: 'docs', // Build to docs folder for GitHub Pages
+    }
 });
